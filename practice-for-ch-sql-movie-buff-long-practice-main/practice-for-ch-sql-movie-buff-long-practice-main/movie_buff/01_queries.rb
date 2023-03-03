9c915810516a2160904a39f2c9976dec21f0d5e6
root <root@DESKTOP-903EI4M.localdomain>
2023-03-03 14:56:34 -0800

    finish phase 1

--- practice-for-ch-sql-movie-buff-long-practice-main/practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/01_queries.rb
+++ practice-for-ch-sql-movie-buff-long-practice-main/practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/01_queries.rb
@@ -43,7 +43,12 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the largest casts (i.e., most
   # actors).
-
+  Movie
+    .joins(:castings)
+    .select(:id, :title)
+    .group("movies.id")
+    .order("COUNT(castings.actor_id) DESC")
+    .limit(3)
 end
 
 def directed_by_one_of(them)
@@ -61,6 +66,10 @@ def directed_by_one_of(them)
 
   # Note: Directors appear in the 'actors' table.
 
+  Movie
+    .select(:id, :title)
+    .joins(:director)
+    .where("actors.name IN (?)", them)
 end
 
 def movie_names_before_1940
@@ -74,5 +83,7 @@ def movie_names_before_1940
   # improve performance for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
-
+  Movie
+    .where('yr < 1940')
+    .pluck(:title)
 end
